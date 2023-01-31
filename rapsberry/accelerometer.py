b9a005fb93a9f52d5af9dd77f994ea95b81d8626
Malachi Paschal <mpascha61@charlottesvilleschools.org>
2023-01-31 13:39:38 -0500

    343347

--- rapsberry/accelerometer.py
+++ rapsberry/accelerometer.py
@@ -8,8 +8,11 @@ sda_pin = (board.GP14)
 scl_pin = (board.GP15)
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
+X = (mpu.acceleration[0])
+Y = (mpu.acceleration[1])
+Z = (mpu.acceleration[2])
 
 while True:
     print(mpu.acceleration)
-    time.sleep(.1)
+    time.sleep(.25)
     #printing the value of the acceleration detected by mpu
